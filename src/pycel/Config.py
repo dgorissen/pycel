396146250227446da21452a5cbf82307f444720c
BenCEEME <benoit.dalferro@gdfsuez.com>
2015-09-22 14:38:08 +0200

    Rangednames management beta
    Add a config file to handle Rangednames as a global variable

---
+++ src/pycel/Config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+try:
+    import numpy as np
+except Exception as e:
+    print "WARNING: cant import dependent numpy packages:",e
+    
+print """I'm in config module"""
+
+rangednames_openpyxl = None
