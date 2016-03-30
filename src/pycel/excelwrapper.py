c1d4235d1b02093cea2eea66e1296f1baf080dd3
Serge Bredin <serge@preuvesetconcepts.com>
2016-03-30 20:05:11 +0200

    move Excel reading from COM object to Openpyxl library

--- src/pycel/excelwrapper.py
+++ src/pycel/excelwrapper.py
@@ -1,127 +1,131 @@
-try:
-    import win32com.client
-    #http://www.py2exe.org/index.cgi/IncludingTypelibs
-    #win32com.client.gencache.is_readonly=False
-    #win32com.client.gencache.GetGeneratePath()
-    from win32com.client import Dispatch
-    from win32com.client import constants 
-    import pythoncom
-    import numpy as np
-except Exception as e:
-    print "WARNING: cant import win32com stuff:",e
+from openpyxl import load_workbook
+from openpyxl.cell import Cell
 
 import os
 from os import path
 
-class ExcelComWrapper(object):
+
+class ExcelWrapper(object):
     
-    def __init__(self, filename, app=None):
+    def __init__(self, filename):
         
-        super(ExcelComWrapper,self).__init__()
+        super(ExcelWrapper,self).__init__()
         
         self.filename = path.abspath(filename)
-        self.app = app
-        
-        # WARNING: by default numpy array require dtype declaration to specify character length (here 'S200', i.e. 200 characters)
-        # WARNING: win32.com cannot get ranges with single column/line, would require way to read Office Open XML
-        # TODO: automate detection of max string length to set up numpy array accordingly
         # TODO: discriminate between worksheet & workbook ranged names
         
+        '''
         self.rangednames = np.zeros(shape = (int(self.app.ActiveWorkbook.Names.Count),1), dtype=[('id', 'int_'), ('name', 'S200'), ('formula', 'S200')])
         for i in range(0, self.app.ActiveWorkbook.Names.Count):
             self.rangednames[i]['id'] = int(i+1)       
             self.rangednames[i]['name'] = str(self.app.ActiveWorkbook.Names.Item(i+1).Name)        
-            self.rangednames[i]['formula'] = str(self.app.ActiveWorkbook.Names.Item(i+1).Value)
-      
-    def connect(self):
-        #http://devnulled.com/content/2004/01/com-objects-and-threading-in-python/
-        # TODO: dont need to uninit?
-        #pythoncom.CoInitialize()
-        if not self.app:
-            self.app = Dispatch("Excel.Application")
-            self.app.Visible = True
-            self.app.DisplayAlerts = 0
-            self.app.Workbooks.Open(self.filename)
-        else:
-            # if we are running as an excel addin, this gets passed to us
-            pass
+           self.rangednames[i]['formula'] = str(self.app.ActiveWorkbook.Names.Item(i+1).Value)
+        '''
+    @property
+    def rangednames(self):
+        if self.workbook == None:
+            return None
+
+        rangednames = []
+
+        for named_range in self.workbook.get_named_ranges():
+            for worksheet, range_alias in named_range.destinations:
+                tuple_name = {
+                    'id': len(rangednames)+1,
+                    'name': str(named_range.name),
+                    'formula': str(worksheet.title+'!'+range_alias)
+                }
+                rangednames.append(tuple_name)
+            
+        return rangednames
     
+
+    def connect(self):
+        self.workbook = load_workbook(self.filename)
+
     def save(self):
-        self.app.ActiveWorkbook.Save()
+        self.workbook.save(self.filename)
     
     def save_as(self, filename, delete_existing=False):
         if delete_existing and os.path.exists(filename):
             os.remove(filename)
-        self.app.ActiveWorkbook.SaveAs(filename)
-  
+        self.workbook.save(filename)
+
     def close(self):
-        self.app.ActiveWorkbook.Close(False)
+        return
   
     def quit(self):
-        return self.app.Quit()
+        return
         
     def set_sheet(self,s):
-        return self.app.ActiveWorkbook.Worksheets(s).Activate()
+        self.workbook.active = s;
+        return self.workbook.active;
     
     def get_sheet(self):
-        return self.app.ActiveWorkbook.ActiveSheet
+        return self.workbook.active
             
     def get_range(self, range):
-        #print '*',range
         if range.find('!') > 0:
             sheet,range = range.split('!')
-            return self.app.ActiveWorkbook.Worksheets(sheet).Range(range)
+            return self.workbook[sheet].iter_rows(range)
         else:        
-            return self.app.ActiveWorkbook.ActiveSheet.Range(range)
+            return self.workbook.active.iter_rows(range)
 
     def get_used_range(self):
-        return self.app.ActiveWorkbook.ActiveSheet.UsedRange
+        return self.workbook.active.iter_rows()
 
     def get_active_sheet(self):
-        return self.app.ActiveWorkbook.ActiveSheet.Name
+        return self.workbook.active.title
     
     def get_cell(self,r,c):
-        return self.app.ActiveWorkbook.ActiveSheet.Cells(r,c)
+        return self.workbook.active.cell(None,r,c)
         
     def get_value(self,r,c):
-        return self.get_cell(r, c).Value
+        return self.get_cell(r, c).value
     
     def set_value(self,r,c,val):
         self.get_cell(r, c).Value = val
 
     def get_formula(self,r,c):
-        f = self.get_cell(r, c).Formula
-        return f if f.startswith("=") else None 
+        cell = self.get_cell(r, c)
+        if cell.data_type in Cell.TYPE_FORMULA:
+            return cell.value
+        else:
+            return None
     
     def has_formula(self,range):
-        f = self.get_range(range).Formula
-        return f and f.startswith("=")
-    
+        tuples = self.get_range(range)
+        for row in tuples:
+            for cell in row:
+                if cell.data_type in Cell.TYPE_FORMULA:
+                    return True
+        return False   
+
     def get_formula_from_range(self,range):
-        f = self.get_range(range).Formula
-        if isinstance(f, (list,tuple)):
-            if any(filter(lambda x: x[0].startswith("="),f)):
-                return [x[0] for x in f];
-            else:
-                return None
-        else:
-            return f if f.startswith("=") else None 
+        formulas = []
+        tuples = self.get_range(range)
+        for row in tuples:
+            for cell in row:
+                if cell.data_type in Cell.TYPE_FORMULA:
+                    formulas.append(cell.value)
+        return formulas    
     
-    def get_formula_or_value(self,name):
-        r = self.get_range(name)
-        return r.Formula or r.Value
+    def get_formula_or_value(self,range):
+        list = []
+        tuples = self.get_range(range)
+        for row in tuples:
+            for cell in row:
+                list.append(cell.value)
+        return list    
 
     def get_row(self,row):
-        return [self.get_value(row,col+1) for col in range(self.get_used_range().Columns.Count)]
+        return [self.get_value(row,col+1) for col in range(self.workbook.active.max_column)]
 
     def set_calc_mode(self,automatic=True):
-        if automatic:
-            self.app.Calculation = constants.xlCalculationAutomatic
-        else:
-            self.app.Calculation = constants.xlCalculationManual
+        raise Exception('Not implemented')
 
     def set_screen_updating(self,update):
-        self.app.ScreenUpdating = update
+        raise Exception('Not implemented')
 
     def run_macro(self,macro):
-        self.app.Run(macro)
+        raise Exception('Not implemented')
---
+++ tests/excelwrapper.py
@@ -0,0 +1,93 @@
+from src.pycel.excelwrapper import ExcelWrapper
+
+import os
+from os import path
+
+
+# RUN AT THE ROOT LEVEL
+excel = ExcelWrapper("./example/example.xlsx")
+
+
+def connect():
+    connected = True
+    try:
+        excel.connect()
+    except Exception as inst:
+        print inst
+        connected = False
+    assert connected == True
+
+def save_as():
+    excel.connect()
+    path_copy = "./example/exampleCopy.xlsx"
+    if os.path.exists(path_copy):
+        os.remove(path_copy)
+    excel.save_as(path_copy)
+    assert os.path.exists(path_copy) == True
+
+def set_and_get_active():
+    excel.connect()
+    excel.set_sheet(2)
+    assert excel.get_sheet().title == "Sheet3"
+
+def get_range():
+    excel.connect()
+    range = excel.get_range('Sheet2!A5:B7')
+    assert sum(map(len,range)) == 6
+
+def get_used_range():
+    excel.connect()
+    assert sum(map(len,excel.get_used_range())) == (excel.get_sheet().max_column * excel.get_sheet().max_row)
+    assert sum(map(len,excel.get_used_range())) == 72
+
+def get_active_sheet():
+    excel.connect()
+    excel.set_sheet(2)
+    assert excel.get_active_sheet() == 'Sheet3'
+
+def get_value():
+    excel.connect()
+    assert excel.get_value(2,2) == "=SUM(A2:A4)"
+
+def get_formula():
+    excel.connect()
+    assert excel.get_formula(2,2) == "=SUM(A2:A4)"
+    assert excel.get_formula(3,12) == None
+
+def has_formula():
+    excel.connect()
+    assert excel.has_formula("Sheet1!C2:C5") == True
+    assert excel.has_formula("Sheet1!A2:A5") == False
+
+def get_formula_from_range():
+    excel.connect()
+    formulas = excel.get_formula_from_range("Sheet1!C2:C5")
+    assert len(formulas) == 4
+    assert formulas[1] == "=SIN(B3*A3^2)"
+
+def get_formula_or_value():
+    excel.connect()
+    list = excel.get_formula_or_value("Sheet1!A2:C2")
+    assert list == [2, '=SUM(A2:A4)', '=SIN(B2*A2^2)']
+
+def get_row():
+    excel.connect()
+    assert len(excel.get_row(2)) == 4
+
+def get_ranged_names():
+    excel.connect()
+    assert excel.rangednames == [{'formula': 'Sheet1!$C$1:$C$18', 'id': 1, 'name': 'SINUS'}]
+
+connect()
+save_as()
+set_and_get_active()
+get_range()
+get_used_range()
+get_active_sheet()
+get_value()
+get_formula()
+has_formula()
+get_formula_from_range()
+get_formula_or_value()
+get_row()
+get_ranged_names()
