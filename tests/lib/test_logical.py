c6292d78e026a61ab196f487acb1e3bfdbcacc8a
Stephen Rauch <stephen.rauch+bitbucket@gmail.com>
2019-09-02 12:41:09 -0700

    Refactor IFS()

--- tests/lib/test_logical.py
+++ tests/lib/test_logical.py
@@ -111,6 +111,30 @@ def test_x_if(test_value, true_value, false_value, result):
 
 
 @pytest.mark.parametrize(
+    'result, value', (
+        (10, (True, 10, True, 20, False, 30)),
+        (20, (False, 10, True, 20, True, 30)),
+        (30, (False, 10, False, 20, True, 30)),
+        (10, ("true", 10, True, 20)),
+        (20, ("false", 10, True, 20)),
+        (10, (2, 10, True, 20)),
+        (20, (0, 10, True, 20)),
+        (10, (2.1, 10, True, 20)),
+        (20, (0.0, 10, True, 20)),
+        (20, (None, 10, True, 20)),
+        (10, (True, 10, "xyzzy", 20)),
+        (VALUE_ERROR, ("xyzzy", 10, True, 20)),
+        (VALUE_ERROR, (tuple(), 10, True, 20)),
+        (DIV0, (DIV0, 10, True, 20)),
+        (NA_ERROR, (False, 10, 0, 20, 'false', 30)),
+        (NA_ERROR, (False, 10, True)),
+    )
+)
+def test_ifs(result, value):
+    assert ifs(*value) == result
+
+
+@pytest.mark.parametrize(
     'result, test_value', (
         (False, True),
         (False, 1),
@@ -173,16 +197,3 @@ def test_x_or(result, test_value):
 )
 def test_x_xor(result, test_value):
     assert x_xor(*test_value) == result
-
-
-@pytest.mark.parametrize(
-    'result, test_value', (
-        (20, (False, 10, True, 20, False, 30)),
-        (NA_ERROR, (False, 10, True)),
-        (VALUE_ERROR, ("fsdfas", 10, True, 20)),
-        (10, (True, 10, "fsdfas", 20)),
-        (NA_ERROR, (False, 10, False, 20, False, 30))
-    )
-)
-def test_ifs(result, test_value):
-    assert ifs(*test_value) == result
