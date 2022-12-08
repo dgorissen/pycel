c5182fec0fff56bcea57962e82d1fe5e286d5e25
Jon Wu <103075869+jonwu95@users.noreply.github.com>
2022-12-08 13:52:27 -0800

    remove version constraint on networkx 2.7

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     tests_require=tests_require,
     test_suite='pytest',
     install_requires=[
-        'networkx>=2.7',
+        'networkx>=2.0',
         'numpy',
         'openpyxl>=2.6.2',
         'python-dateutil',
